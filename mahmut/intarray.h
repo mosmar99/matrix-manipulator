21ca3abf0d2e43280978f2cad9dadb353269b057
MahmutOsmanovic <47375043+MahmutOsmanovic@users.noreply.github.com>
2021-11-22 18:02:48 +0100

    up

--- mahmut/intarray.h
+++ mahmut/intarray.h
@@ -1,5 +1,7 @@
-// Comment
-
+// Module contains useful functions for working with integer arrays that are 
+// allocated on the program's stack memory. All elements in the array are positive
+// integers, with the exception of one special element with the value -1, which acts
+// a SENTINEL (terminator) for the end of the array (compare to \0 for strings)
 
 #ifndef _intarray_h
 #define _intarray_h
